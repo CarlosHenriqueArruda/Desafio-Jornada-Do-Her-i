e5d4073f4fc6d8db10a3f6af1859fc0dcc863883
carloshenriquearruda <carloshenriquearruda82@gmail.com>
2023-12-05 13:37:17 -0300

    commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let nome = "Sawyer"
+let xp = 10001
+let nivel
+
+switch (true) {
+        case xp < 1000:
+            nivel = 'Ferro';
+            break;
+        case xp >= 1001 && xp <= 2000:
+            nivel = 'Bronze';
+            break;
+        case xp >= 2001 && xp <= 5000:
+            nivel = 'Prata';
+            break;
+        case xp >= 6001 && xp <= 7000:
+            nivel = 'Ouro';
+            break;
+        case xp >= 7001 && xp <= 8000:
+            nivel = 'Platina';
+            break;
+        case xp >= 8001 && xp <= 9000:
+            nivel = 'Ascendente';
+            break;
+        case xp >= 9001 && xp <= 10000:
+            nivel = 'Imortal';
+            break;
+        case xp >= 10001:
+            nivel = 'Radiante';
+            break;
+        default:
+            nivel = 'Nível não definido';
+}
+console.log(`O Herói de nome '${nome}' está no nível: ${nivel}`)
+
